b1bc431ec71f459c450121d47d23640e5f33a8c6
Mst Rinky Akter <mstrinkya@gmail.com>
2023-03-18 23:39:36 +0600

    tempo and distucturing

---
+++ swap.js
@@ -0,0 +1,11 @@
+let first = 5;
+let second = 7;
+console.log(first,second);
+// const tempo = first;
+// first = second;
+// second = tempo;
+// console.log(first,second);
+
+// distructuring ----------
+[first,second] = [second,first];
+console.log(first,second);
