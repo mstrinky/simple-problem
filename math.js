4ddd3719dbecd801b51193c5b8544d0fe2e5f163
Mst Rinky Akter <mstrinkya@gmail.com>
2023-03-18 22:36:23 +0600

    abs and pow

--- math.js
+++ math.js
@@ -1,1 +1,14 @@
-console.log('hello world');
+// console.log('hello world');
+const result = Math.pow(2,3);
+console.log(result);
+
+const myYear = 21;
+const hisYear = 23;
+const gap =Math.abs (myYear - hisYear);
+// console.log (gap);
+if(gap<=2){
+    console.log('perfect match');
+}
+else{
+    console.log('not perfect match');
+}
