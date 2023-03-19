8550886bb5ce117d5000a0c60d84e75181ee99c0
Mst Rinky Akter <mstrinkya@gmail.com>
2023-03-19 11:01:19 +0600

    find the max number

---
+++ max2.js
@@ -0,0 +1,15 @@
+const first = 55;
+const second = 78;
+const third = 60;
+if (first>second && first>third){
+    console.log('first is the max number');
+}
+else if(second>first && second>third){
+    console.log('second is the max number');
+}
+else{
+    console.log('third is the max number');
+}
+
+var findMax = Math.max(4,5,6);
+console.log(findMax);
