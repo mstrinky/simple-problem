4555531b1883bcdea72994e632f38496de9e1172
Mst Rinky Akter <mstrinkya@gmail.com>
2023-03-19 11:52:52 +0600

    find heighest number

---
+++ heighest.js
@@ -0,0 +1,15 @@
+function lowestInArray(numbers){
+    let heighestNumber =[0];
+for(let i=0; i<numbers.length; i++){
+    const index = i;
+    const element = numbers[index];
+    console.log(element);
+    if(element > heighestNumber){
+        heighestNumber = element;
+    }
+}
+return heighestNumber;
+}
+const heights = [167,190,120,165,137,265];
+const heighest = lowestInArray(heights);
+console.log('the heighest number is :',heighest);
